c74044d4f32de5d1ec21a50941bcbef3b5f3d58e
ochi <ocos54ce@gmail.com>
2024-06-07 19:19:16 +0900

    backend

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+const prisma = new PrismaClient();
+
+app.use(cors());
+
+app.get("/", (req, res) => res.redirect("/todos"));
+
+app.get("/todos", async (req, res) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const todos = await prisma.todo.findMany();
+  // return res.json(todos);
+
+  return res.status(500).json({ message: "Internal Server Error" });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
